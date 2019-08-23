d5d3783235871df2d03bf6a323c9acda8cd3c620
Tomas <tomas.storc@gmail.com>
2019-08-23 11:33:03 +0200

    almost there

--- proc.py
+++ proc.py
@@ -1,5 +1,39 @@
-#!/usr/bin/python3
 import psutil
-for proc in psutil.process_iter():
-    if proc.name() in "firefox":
-        print("je tam")
+import os
+import socket
+
+host = socket.gethostname()
+running = []
+ptime = []
+def getproc(name):
+	for proc in psutil.process_iter():
+		if proc.name() in name:
+			running.append(name)
+
+def genproc():
+	procs = ['ansa_linux_x86_64', 'meta_post_x86_64', 'a4']
+	for getp in procs:
+		getproc(getp)
+
+
+
+def selectproc():
+	if 'a4' in running:
+		animator = 'animator - {}'.format(host)
+		print(animator)
+
+	if 'ansa_linux_x86_64' in running:
+		ansa = 'ansa - {}'.format(host)
+		print(ansa)
+
+	if 'meta_post_x86_64' in running:
+		meta = 'meta - {}'.format(host)
+		print(meta)
+		
+def main():
+
+		genproc()
+		selectproc()
+
+if __name__ == "__main__":
+	main()
