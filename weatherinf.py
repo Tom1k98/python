fe5567cee40f32a94fa7f267871c522c21de336e
Tomas Storc <tomas.storc@gmail.com>
2019-09-01 21:38:17 +0200

    changed vars name

--- weatherinf.py
+++ weatherinf.py
@@ -21,15 +21,11 @@ def getvalues():
     weat = requests.get('https://pocasi.seznam.cz/praha?lat=50.0848&lon=14.4112&z=5&f=muni').text
     data = BeautifulSoup(weat, 'lxml')
     final = data.find('button', {'class': 'd_gQ'})
-    dnestt = final.div.text
-    dnest = re.sub('[^0-9/]+', '', dnestt).split('/')
-    max = dnest[0]
-    min = dnest[1]
+    dnes = final.div.text
+    dnes = re.sub('[^0-9/]+', '', dnes).split('/')
+    max = dnes[0]
+    min = dnes[1]
     print('maximalni teplota dnes {} bude {}, nejnizsi denni teplota bude {}'.format(date, max, min))
-    with open('/home/tom/data/pocasi', 'a') as f:
-            f.write('{} - max - {}\n'.format(date, max))
-            f.write('{} - min - {}\n'.format(date, min))
-    f.close()
 
     with open('/home/tom/data/pocasi.csv', 'a') as csv_file:
         csv_w = csv.writer(csv_file)
