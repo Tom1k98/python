766c40b2e6ddbb8527f7dcae79de090c50e73637
Tomas <tomas.storc@gmail.com>
2019-09-16 19:45:09 +0200

    drop duplicates from csv

---
+++ makefinal.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+IN_FILE = 'novinky.csv'
+OUT_FILE = 'final.csv'
+
+df = pd.read_csv(IN_FILE, sep=',', encoding='utf-8')
+df.columns = ['datum', 'titulek', 'odkaz', 'komentare', 'text']
+
+df.drop_duplicates(subset='titulek',inplace = True, keep = 'last')
+df.to_csv(OUT_FILE, encoding='utf-8')
