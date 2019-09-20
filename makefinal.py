ac811ac359af4159a94e326bce283ae54a1065de
Tomas <tomas.storc@gmail.com>
2019-09-20 12:09:23 +0200

    added rubrika column

--- makefinal.py
+++ makefinal.py
@@ -4,7 +4,7 @@ IN_FILE = 'novinky.csv'
 OUT_FILE = 'final.csv'
 
 df = pd.read_csv(IN_FILE, sep=',', encoding='utf-8')
-df.columns = ['datum', 'titulek', 'odkaz', 'komentare', 'text']
+df.columns = ['datum', 'titulek', 'odkaz', 'komentare', 'text', 'rubrika']
 
 df.drop_duplicates(subset='titulek',inplace = True, keep = 'last')
 df.to_csv(OUT_FILE, encoding='utf-8')
