1428b4bd9cc82c916ea09159351521fef560dd53
Tomas <tomas.storc@gmail.com>
2019-09-28 09:08:07 +0200

    removed download added http iframe and db

--- getvideo.py
+++ getvideo.py
@@ -8,8 +8,7 @@ import time
 from mymail import *
 import concurrent.futures
 import unidecode
-
-TARGET = 'C:/Users/Tomas/Videos'
+import sqlite3
 
 channels = ['https://www.youtube.com/channel/UCwmiCqC-56bUIdWBd9hTu2g',
 'https://www.youtube.com/user/AlexandrasGirlyTalk/videos',
@@ -20,10 +19,7 @@ channels = ['https://www.youtube.com/channel/UCwmiCqC-56bUIdWBd9hTu2g',
 'https://www.youtube.com/user/BeautyBySiena/videos']
 
 newvid = []
-
-def out(command):
-    result = run(command, stdout=PIPE, stderr=PIPE, universal_newlines=True, shell=True)
-    return result.stdout
+linktovid = []
 
 def getvid(url):
     site = url
@@ -33,15 +29,16 @@ def getvid(url):
     nazevt = div.h3.a['title']
     linkt = div.h3.a['href']
     nazev = re.sub('[[?!|\*]', '', unidecode.unidecode(nazevt)).replace(' ', '_').replace('&', 'and')
-    print('nazev: {}'.format(nazevt))
-    print(f'odkaz: https://www.youtube.com{linkt}')
     link = f'https://www.youtube.com{linkt}'
-    if os.path.isfile('{}/{}'.format(TARGET, nazev)):
-        print('Skipping, already downloaded')
+    lframe_tmp = linkt.split('=')[1]
+    link_frame = f'https://www.youtube.com/embed/{lframe_tmp}'
+    if isVideo(nazev):
+        print('Skipping, already added')
     else:
-        print(f"Downloading {nazev}")
-        out(f'youtube-dl -f \"bestvideo[ext=mp4],bestaudio\" {link} -o {TARGET}/{nazev}')
+        print(f"Adding {nazevt}")
+        addVideo(nazev)
         newvid.append(f"nazev: {nazevt}\n")
+        linktovid.append(f"<iframe width=\"420\" height=\"315\" src={link_frame}></iframe>")
 
 
 def sendinfo(to):
@@ -50,13 +47,54 @@ def sendinfo(to):
     subject = 'nova videa'
     mysendmail(to, subject, message)
 
+def makeHtml():
+    start_html = '<html><head></head><body>'
+    frames_tmp = ''.join(linktovid)
+    end_html = '</body></html>'
+    frames = f'{start_html} {frames_tmp} {end_html}'
+    with open('index.html', 'w') as html_file:
+        html_file.write(frames)
+
+def addVideo(vid):
+    conn = sqlite3.connect('videos.db')
+    c = conn.cursor()
+    add = """
+    INSERT INTO videos (nazev) VALUES (?)
+    """
+    c.execute(add, (vid, ))
+    conn.commit()
+    conn.close()
+
+def isVideo(vid):
+    conn = sqlite3.connect('videos.db')
+    c = conn.cursor()
+    sql = """
+    SELECT * FROM videos WHERE nazev=?
+    """
+    c.execute(sql, (vid,))
+    return True if len(c.fetchall())>0 else False
+
+def makeTable():
+    conn = sqlite3.connect('videos.db')
+    c = conn.cursor()
+    table = """
+    CREATE TABLE IF NOT EXISTS videos (
+        id integer PRIMARY KEY AUTOINCREMENT,
+        nazev text
+    )
+    """
+    c.execute(table)
+    conn.commit()
+    conn.close()
 
 
 if __name__ == "__main__":
+    makeTable()
     start = time.perf_counter()
     with concurrent.futures.ThreadPoolExecutor() as executor:
         executor.map(getvid, channels)
 
     finish = time.perf_counter()
     print(f"Finished in {round(finish-start, 2)} second(s)")
-    #sendinfo('petriciecavojova@seznam.cz')
+    sendinfo('tomas.storc@afd.cz')
+    makeHtml()
