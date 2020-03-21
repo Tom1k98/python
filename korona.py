a6f06f493a5de1b3614a9d0cb41a62f4e68a9ddf
Tomáš Štorc <tom@kstot01.knet.vse.cz>
2020-03-21 09:52:26 +0100

    print cases and recovered

--- korona.py
+++ korona.py
@@ -1,4 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
-# test 
+import lxml
+
 url = requests.get('https://onemocneni-aktualne.mzcr.cz/covid-19')
+soup = BeautifulSoup(url.text, 'lxml')
+
+def casesNum():
+    getNumC = soup.find('p', {'id': 'count-sick'})
+    return getNumC.text
+def recoverNum():
+    getNumR = soup.find('p', {'id': 'count-recover'})
+    return getNumR.text
+if __name__ == "__main__":
+    print(f'{casesNum()} - {recoverNum()}')
