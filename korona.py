0754f65bd6c4b3542660453b6826d45eb3a2a1b5
Tomáš Štorc <tom@kstot01.knet.vse.cz>
2020-03-21 10:15:21 +0100

    write data to csv file

--- korona.py
+++ korona.py
@@ -1,15 +1,33 @@
+#!/usr/bin/python3
 import requests
 from bs4 import BeautifulSoup
 import lxml
+from datetime import datetime
+
+OUT_FILE = '/Users/tom/Library/Mobile Documents/com~apple~Numbers/Documents/cases.csv'
 
 url = requests.get('https://onemocneni-aktualne.mzcr.cz/covid-19')
 soup = BeautifulSoup(url.text, 'lxml')
 
+now = datetime.now()
+time = now.strftime("%d.%m.%Y %H:%M")
+
 def casesNum():
     getNumC = soup.find('p', {'id': 'count-sick'})
     return getNumC.text
+
 def recoverNum():
     getNumR = soup.find('p', {'id': 'count-recover'})
     return getNumR.text
+
+def testsNum():
+    getNumT = soup.find('p', {'id': 'count-test'})
+    return getNumT.text
+
+def writeToFile():
+    with open(OUT_FILE, 'a+') as file:
+        file.write(f'{time},{testsNum()},{casesNum()},{recoverNum()}')
+
 if __name__ == "__main__":
-    print(f'{casesNum()} - {recoverNum()}')
+    #print(f'{time} - {testsNum()} - {casesNum()} - {recoverNum()}')
+    writeToFile()
