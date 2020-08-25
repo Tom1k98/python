b3a825d3d669d9b24cecc754ebe1c8966077d799
Tomáš Štorc <tom@tmsz-main.local>
2020-08-25 13:47:11 +0200

    changed out line

--- backupifs.py
+++ backupifs.py
@@ -3,8 +3,11 @@ import tarfile
 from subprocess import PIPE
 from datetime import datetime
 
+PATH_IN = '/Users/tom'
+PATH_OUT = ''
+
 atm = datetime.now()
-TIME_STAMP = atm.strftime("%d%m%y%H%M")
+TIME_STAMP = atm.strftime("%d%m%y")
 
 def findFiles(vmname):
-    return subprocess.run(['find', 'snapshot-create-as', '--domain', vmname, '--name', bc_name], stderr=PIPE, stdout=PIPE)
+    return subprocess.run(['find', PATH_IN, '-daystart', '-cstart', '0', '-print'], stderr=PIPE, stdout=PIPE)
