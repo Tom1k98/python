ac221bf0e15b8bbf8ee112f849383bc7d0970111
Tom1k98 <tomas.storc@gmail.com>
2020-08-25 17:18:44 +0200

    backup script

--- backupifs.py
+++ backupifs.py
@@ -2,12 +2,38 @@ import subprocess
 import tarfile
 from subprocess import PIPE
 from datetime import datetime
+import logging
 
-PATH_IN = '/Users/tom'
-PATH_OUT = ''
+PATH_IN = '/oracle/FRA/PROD/backup'
+PATH_OUT = '/mnt/IKOS-EXPORT/ifs_bc'
+LOG_FILE = '/var/log/ifs_backup.log'
 
 atm = datetime.now()
 TIME_STAMP = atm.strftime("%d%m%y")
 
-def findFiles(vmname):
-    return subprocess.run(['find', PATH_IN, '-daystart', '-cstart', '0', '-print'], stderr=PIPE, stdout=PIPE)
+
+def findFiles():
+   return subprocess.run(['find', PATH_IN, '-type', 'f', '-mtime', '-1', '-print'], stderr=PIPE, stdout=PIPE).stdout.decode('UTF-8').split('\n')
+
+def makeTar(files):
+    tar_name = f'{PATH_OUT}/ifsbc_{TIME_STAMP}.tar.gz'
+    logging.info(f'creating new tar file {tar_name}')
+    with tarfile.open(tar_name, 'w:gz') as tar:
+        for file in files:
+            logging.info(f'adding file {file} to {tar_name}')
+            tar.add(file)
+            logging.info(f'file {file} added successfully')
+
+
+if __name__ == '__main__':
+    logging.basicConfig(filename=LOG_FILE,
+                    filemode='a',
+                    level=logging.DEBUG,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
+    files = findFiles()
+    del files[-1]
+    logging.info('files saved to list')
+    makeTar(files)
+    logging.info('tar created')
+    
